140de2eacd8544eeed6d2a632bfdf853ee3d266c
rohit chavan <r.rohitchavan724@gmail.com>
2025-02-18 16:52:39 +0530

    frontend intergration

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { useAuth } from "./context/auth";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 
-const Home = ()=>{
-const [auth,setAuth] = useAuth();
+const Home = () => {
+    //context
+    const [auth, setAuth] = useAuth();
+
+    const [adsForSell, setAdsForSell] = useState('');
+    const [adsForRent, setAdsForRent] = useState('');
+
+    useEffect(() => {
+        fetchAllAds()
+    }, []);
+
+    const fetchAllAds = async () => {
+        const { data } = await axios.get('/ads');
+        setAdsForRent(data.adsForRent)
+        setAdsForSell(data.adsForSell)
+    }
 
     return (
         <>
-        <h1 className="display-p1 text-light bg-primary">Home</h1>
-    
+            <h1 className="display-p1 text-light bg-primary">Home</h1>
+            
+
         </>
     )
 }
