e42d578d8d1c2248106a0325e9cff54e40ec1077
rohit chavan <r.rohitchavan724@gmail.com>
2025-02-24 18:12:32 +0530

    frontend integration with ads api

--- client/src/components/AdView.js
+++ client/src/components/AdView.js
@@ -1,10 +1,31 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-const AdView = ()=>{
-const params = useParams();
-    return(
+import axios from "axios";
+import { useEffect, useState } from "react";
+const AdView = () => {
+    const {slug} = useParams();
+    const [ad, setAd] = useState([]);
+    const [related, setRelated] = useState([]);
+    useEffect(() => {
+     
+        const fetchAd = async () => {
+            try {
+                const { data } = await axios.get(`/ads/${slug}`);
+                setAd(data.ad);
+                setRelated(data.related);
+                console.log(data)
+            } catch (err) {
+                console.log(err, 'Error in fetching the API')
+            }
+           
+        }
+        if (slug) {
+            fetchAd();
+        }
+    }, [slug])
+    return (
         <>
-<pre>{params.slug}</pre>
+            <pre>{JSON.stringify({ad,related},null,4)}</pre>
         </>
     )
 }
